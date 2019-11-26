b1f2179e03682eb7b652f67ef843bf97f6349c36
Karhu Janne TTK18SP <jannekarhu2@kamk.fi>
2019-11-26 09:58:49 +0200

    Dice are rolling

--- bot.js
+++ bot.js
@@ -20,33 +20,46 @@ bot.on('message', function (user, userID, channelID, message, evt) {
     // Our bot needs to know if it will execute a command
     // It will listen for messages that will start with `!`
     if (message.substring(0, 1) == '!') {
-        var args = message.substring(1).split(' ');
+        var args = message.substring(1).split('d');
         var cmd = args[0];
-       
+		var a = args[0];
+		var d = args[1];
         args = args.splice(1);
-        switch(cmd) {
-            // !ping
-            case 'ping':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'Pong!'
-                });
-            break;
-            // Just add any case commands if you want to..
-         }
+        var result = [0];
+		amount = a;
+		dice = d;
+		modifier = m;
+		var sum =0;
+		
+		for(i = 0; i < amount; i++)
+		{
+			result[i] = Math.floor(Math.random() * dice - 0 + 1);
+			
+		}
+		
+		if(result.length >= 1)
+		{
+			
+			for(i = 0; i < result.length; i++)
+			{
+				sum += result[i];
+				
+			}
+			
+		}
+		
+		var output = result.toString() + " = " + sum.toString();
+			
+		
+		
+		bot.sendMessage({
+			to: channelID,
+			message: output
+		});
+         
      }
 });
-/*function rollTheDice(int amount, int dice)
-{
-	var result[];
-	for(int i = 0; i < amount; i++)
-	{
-		result[i] = Math.floor(Math.random() * dice - 0 + 1);
-		
-	}
-	var output = result.toString();
-	return output;
-}*/
+
 
 
 
